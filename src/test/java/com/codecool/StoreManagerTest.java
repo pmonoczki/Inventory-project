ba63a1fe361eafbd987c20915fc2fac087a1f26e
Pál Monoczki <pal.monoczki@codecool.com>
2019-01-27 20:30:27 +0100

    Product anstract class has been added.

---
+++ src/test/java/com/codecool/StoreManagerTest.java
@@ -0,0 +1,33 @@
+package com.codecool;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addStorage() {
+    }
+
+    @Test
+    void getTotalPrice() {
+    }
+
+    @Test
+    void addCDProduct() {
+    }
+
+    @Test
+    void addBookProduct() {
+    }
+
+    @Test
+    void listProducts() {
+    }
+}
