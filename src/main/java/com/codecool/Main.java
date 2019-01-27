f31ed1f7441137470353e39618b7300492416cae
Pál Monoczki <pal.monoczki@codecool.com>
2019-01-27 18:32:09 +0100

    Product anstract class has been added.

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,7 +1,23 @@
+/**
+ * @author Pal Monoczki
+ * @version 1.0
+ */
+
 package com.codecool;
 
+/**
+ *
+ * Lods the inventory project
+ * @param args controls the program by arguments
+ */
 public class Main {
     public static void main(String[] args){
-        
     }
 }
+
+
+
+
+
+
+
---
+++ src/main/java/com/codecool/Product.java
@@ -0,0 +1,22 @@
+package com.codecool;
+
+public abstract class Product {
+
+    private String name;
+
+    private int price;
+
+    public Product(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+}
