13ae913ab59c9490dbf7f0b2d30cecb27ec1d11d
Pál Monoczki <pal.monoczki@codecool.com>
2019-01-27 19:08:07 +0100

    Product anstract class has been added.

---
+++ src/test/java/com/codecool/CDProductTest.java
@@ -0,0 +1,18 @@
+package com.codecool;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CDProductTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumOfTracks() {
+    }
+}
