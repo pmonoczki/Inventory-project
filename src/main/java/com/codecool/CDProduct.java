745b2ed24a83fa4f262a3ce666d9175ef49883b0
Pál Monoczki <pal.monoczki@codecool.com>
2019-01-27 18:58:49 +0100

    Product anstract class has been added.

--- src/main/java/com/codecool/CDProduct.java
+++ src/main/java/com/codecool/CDProduct.java
@@ -1,4 +1,15 @@
 package com.codecool;
 
-public class CDProduct {
+public class CDProduct extends Product{
+
+    private int numOfTracks;
+
+    public CDProduct(String name, int price, int numOfTracks){
+        super(name, price);
+        this.numOfTracks = numOfTracks;
+    }
+
+    public int getNumOfTracks(){
+        return  this.numOfTracks;
+    }
 }
---
+++ src/test/java/CDProductTest.java
@@ -0,0 +1,2 @@
+public class CDProductTest {
+}
